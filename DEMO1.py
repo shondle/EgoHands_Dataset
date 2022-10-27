a376cde68271e5cfeaf0b928ad0305f6fc3ad6b2
shondle <106922533+shondle@users.noreply.github.com>
2022-10-27 17:06:44 -0500

    Update DEMO1.py

--- DEMO1.py
+++ DEMO1.py
@@ -9,7 +9,17 @@ videos = getMetaBy('Location', 'COURTYARD', 'Activity', 'PUZZLE')
 img = cv2.imread(str(getFramePath(videos.iloc[0], 7)))
 
 hand_mask = getSegmentationMask(videos.iloc[0], 7, 'all')
+bounding_boxes = getBoundingBoxes(videos.iloc[0], 7)
 
-cv2.imshow('hi', hand_mask)
+cv2.namedWindow('window', cv2.WINDOW_NORMAL)
+cv2.imshow('window', hand_mask)
 cv2.waitKey(0)
 
+# testing rectangles from getBoundingBoxes
+rect = cv2.rectangle(img, np.int32(bounding_boxes[0]), (255, 255, 255), 3)
+rect = cv2.rectangle(img, np.int32(bounding_boxes[1]), (255, 255, 255), 3)
+rect = cv2.rectangle(img, np.int32(bounding_boxes[2]), (255, 255, 255), 3)
+rect = cv2.rectangle(img, np.int32(bounding_boxes[3]), (255, 255, 255), 3)
+
+cv2.imshow('window', rect)
+cv2.waitKey(0)
