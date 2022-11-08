f607ec97cef79874beb73c4716222600fb6df9de
shondle <106922533+shondle@users.noreply.github.com>
2022-11-07 18:40:23 -0600

    Update getFramePath.py

--- getFramePath.py
+++ getFramePath.py
@@ -1,5 +1,13 @@
 from pathlib import Path
 
+#  getFramePath(vid, i) returns the file path to the "i"th labeled frame in video "vid", where "vid"
+#  is an EgoHands video metadata structure.
+#
+#
+#    For full dataset details, see the <a href="matlab: web('http://vision.soic.indiana.edu/egohands')">EgoHands project website</a>.
+#
+#    See also getBoundingBoxes, getMetaBy, getSegmentationMask, showLabelsOnFrame
+
 def getFramePath(video, i):
     # change path to generic before sending
     base_path = Path(r'.\egohands_data\_LABELLED_SAMPLES'
