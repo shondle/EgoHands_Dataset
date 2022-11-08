3bcfa393eb51f5fe89de3d3cb17b74c3b8cae4a8
shondle <106922533+shondle@users.noreply.github.com>
2022-11-07 18:43:36 -0600

    Update visualizeData.py

--- visualizeData.py
+++ visualizeData.py
@@ -3,15 +3,28 @@ from getMetaBy import getMetaBy
 from matplotlib import pyplot as plt
 import math
 
+# This is an example of how to create and visualize a PyTorch dataset object
+# with respective colored images and hand segmentation labels for the
+# EgoHands dataset videos.
+
+# this is to query the videos. For more info, check out getMetaBy.py
 videos = getMetaBy('Location', 'COURTYARD', 'Activity', 'PUZZLE')
 
+# create the dataset object
 dataset = EgoHandsDataset(videos, None)
+
+
+# create a figure to display all the images and segmentation masks
+# from the dataset you just created. This displays all frames' images and masks
+# from all videos queried, so it is more of just a way to debug. Don't rely on
+# this for large databases, or adjust the code if you do decide to do so.
 
 figure = plt.figure(figsize=(10,10))
 totalImgs = len(dataset.masks) + len(dataset.images)
 cols, rows = math.ceil(math.sqrt(totalImgs)), math.ceil(math.sqrt(totalImgs))
 count = 1
 i = 1
+
 while i < len(range(1, cols * rows + 1)):
     img, mask = dataset.__getitem__(count)
 
