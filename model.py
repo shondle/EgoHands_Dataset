d7511ac7bd6829dad6dc30d259af8498f4a70365
shondle <106922533+shondle@users.noreply.github.com>
2022-11-17 08:44:13 -0600

    Update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.LazyLinear(120)
+        # self.fc1 = nn.LazyLinear(120)
         # self.adapt = nn.AdaptiveMaxPool2d((5, 7))
         self.fc1 = nn.Linear(703, 120)
         # self.fc1 = nn.Linear(16 * 5 * 5, 120)
